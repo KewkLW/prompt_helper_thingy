78be773118f82019218602602c81d613162d7575
Kewk <57611539+KewkLW@users.noreply.github.com>
2023-08-31 13:56:03 -0700

    Add files via upload

--- prompt_helper_thingy.py
+++ prompt_helper_thingy.py
@@ -1,9 +1,7 @@
 import gradio as gr
 from tkinter import filedialog
 from tkinter import Tk
-from tqdm import tqdm
-import os
-#iAm_Kewk
+#iAmKewk
 
 def generate_and_save_prompts(prompt, prepend, append, args):
     # Initialize list to hold final prompts
@@ -15,9 +13,7 @@ def generate_and_save_prompts(prompt, prepend, append, args):
     append_lines = [line for line in append.split("\n") if line.strip() != ""]
     args_lines = [line for line in args.split("\n") if line.strip() != ""]
     
-    print("Generating prompts...")
-    
-    for pre in tqdm(prepend_lines):
+    for pre in prepend_lines:
         for main in prompt_lines:
             for app in append_lines:
                 for arg in args_lines:
@@ -28,20 +24,16 @@ def generate_and_save_prompts(prompt, prepend, append, args):
     root.withdraw()
     
     # Open file explorer and get the save path
-    print("Opening file explorer...")
     file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
     
     if file_path:
-        print(f"Saving to {file_path}...")
         # Save the final prompts to a text file
         with open(file_path, "w") as f:
             for line in final_prompts:
                 f.write(f"{line}\n")
                 
-        print(f"Saved prompts to {file_path}")
         return f"Prompts saved to {file_path}"
     else:
-        print("File save cancelled.")
         return "File save cancelled."
 
 # Define Gradio interface
